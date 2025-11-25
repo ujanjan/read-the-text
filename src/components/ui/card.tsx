2a89f6b33b5bccc025e97840b2b66467b2a686b7
ujanjan <ujanjan@gmail.com>
2025-11-25 12:22:57 +0100

    fix CSS

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
         ref={ref}
         data-slot="card"
         className={cn(
-          "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
+          "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border shadow-xs",
           className,
         )}
         {...props}
